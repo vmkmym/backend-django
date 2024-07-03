93dbfb2aca2419772485705cdd97f78b3fbe66e3
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-07-04 04:43:26 +0900

    Update models.py

--- meteo/models.py
+++ meteo/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Worldcities(models.Model):
-    id = models.AutoField(primary_key=True)  # primary_key=True를 추가
+    id = models.AutoField(primary_key=True)
     city = models.TextField(blank=True, null=True)
-    lat = models.TextField(blank=True, null=True)  # This field type is a guess.
-    lng = models.TextField(blank=True, null=True)  # This field type is a guess.
+    lat = models.TextField(blank=True, null=True)  
+    lng = models.TextField(blank=True, null=True) 
     country = models.TextField(blank=True, null=True)
 
     class Meta:
